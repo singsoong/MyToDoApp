1de961843943fd1399b0f5272f12310f69efaa48
HJRYU\o__hy <o__hyeah@naver.com>
2023-06-28 17:05:27 +0900

    refatctor: Todo, ToDoList 컴포넌트 분리

--- src/components/ToDoList.tsx
+++ src/components/ToDoList.tsx
@@ -1,47 +1,16 @@
-import { useForm } from "react-hook-form";
-import { useRecoilState, useRecoilValue } from "recoil";
+import { useRecoilValue } from "recoil";
+import CreateToDo from "./CrateToDo";
 import { toDoState } from "../recoil";
-
-interface IForm {
-  toDo: string;
-}
+import ToDo from "./Todo";
 
 export default function ToDoList() {
-  const [toDos, setToDos] = useRecoilState(toDoState);
-  const {
-    register,
-    handleSubmit,
-    setValue,
-    formState: { errors },
-  } = useForm<IForm>();
-
-  const onValid = ({ toDo }: IForm) => {
-    setToDos((oldToDos) => [
-      { id: Date.now(), text: toDo, category: "TO_DO" },
-      ...oldToDos,
-    ]);
-    setValue("toDo", "");
-  };
-
+  const toDos = useRecoilValue(toDoState);
   return (
     <>
       <h1>To Dos</h1>
       <hr />
-      <form onSubmit={handleSubmit(onValid)}>
-        <input
-          {...register("toDo", {
-            required: "required plz",
-          })}
-          placeholder="Write a to do"
-        />
-        <button>Add</button>
-        <span>{errors?.toDo?.message as string}</span>
-      </form>
-      <ul>
-        {toDos?.map((todo) => {
-          return <li key={todo.id}>{todo.text}</li>;
-        })}
-      </ul>
+      <CreateToDo />
+      <ul></ul>
     </>
   );
 }
--- src/recoil/toDo.ts
+++ src/recoil/toDo.ts
@@ -1,6 +1,6 @@
 import { atom } from "recoil";
 
-interface IToDo {
+export interface IToDo {
   id: number;
   text: string;
   category: "TO_DO" | "DOING" | "DONE";
