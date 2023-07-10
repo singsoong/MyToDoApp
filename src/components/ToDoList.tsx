6de00939c46baec9ddd762fcd45f4a5fa42b44b6
HJRYU\o__hy <o__hyeah@naver.com>
2023-07-10 14:39:24 +0900

    feat: Recoil, Selector 1

--- src/components/ToDoList.tsx
+++ src/components/ToDoList.tsx
@@ -1,18 +1,33 @@
 import { useRecoilValue } from "recoil";
 import CreateToDo from "./CrateToDo";
-import { toDoState } from "../recoil";
+import { toDoSelector } from "../recoil";
 import ToDo from "./Todo";
 
 export default function ToDoList() {
-  const toDos = useRecoilValue(toDoState);
+  const [toDo, done, doing] = useRecoilValue(toDoSelector);
   return (
     <>
       <h1>To Dos</h1>
-      <hr />
       <CreateToDo />
+      <hr />
+      <h2>To Do</h2>
       <ul>
-        {toDos.map((toDo) => (
-          <ToDo key={toDo.id} {...toDo} />
+        {toDo.map((item) => (
+          <ToDo key={item.id} {...item} />
+        ))}
+      </ul>
+      <hr />
+      <h2>Doing</h2>
+      <ul>
+        {doing.map((item) => (
+          <ToDo key={item.id} {...item} />
+        ))}
+      </ul>
+      <hr />
+      <h2>Done</h2>
+      <ul>
+        {done.map((item) => (
+          <ToDo key={item.id} {...item} />
         ))}
       </ul>
     </>
