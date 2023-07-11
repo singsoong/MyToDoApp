c7f2ad362d55af19a653e540103192d5c31d2d0d
HJRYU\o__hy <o__hyeah@naver.com>
2023-07-11 10:36:32 +0900

    feat: Recoil get, set practice - Hour ↔ Minute Conversion

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,33 @@
 import React from "react";
-import ToDoList from "./components/ToDoList";
+import { useRecoilState } from "recoil";
+import { hourSelector, minuteState } from "./recoil/atoms";
 
 function App() {
+  const [minutes, setMinutes] = useRecoilState(minuteState);
+  const [hours, setHours] = useRecoilState(hourSelector);
+
+  const onMinutesChanges = (event: React.FormEvent<HTMLInputElement>) => {
+    setMinutes(Number(event.currentTarget.value));
+  };
+
+  const onHoursChange = (event: React.FocusEvent<HTMLInputElement>) => {
+    setHours(Number(event.currentTarget.value));
+  };
+
   return (
     <>
-      <ToDoList />
+      <input
+        value={minutes}
+        onChange={onMinutesChanges}
+        type="number"
+        placeholder="Minutes"
+      />
+      <input
+        value={hours}
+        onChange={onHoursChange}
+        type="number"
+        placeholder="Hours"
+      />
     </>
   );
 }
